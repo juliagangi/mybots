5a8ce9398849015796d6c677cc06106fe50453d5
Julia Gangi <juliagangi01@gmail.com>
2023-01-08 22:13:15 -0600

    refactoring

--- simulate.py
+++ simulate.py
@@ -4,35 +4,31 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
 import random
-import matplotlib.pylab as plt
+import constants as c
 
+simulation = SIMULATION()
 
+"""
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
-
 pyrosim.Prepare_To_Simulate(robotId)
+
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
 
-amplitudeF = numpy.pi/14
-frequencyF = 30
-phaseOffsetF = 0
 motorVectorF = numpy.linspace(0,2*numpy.pi,1000)
 targetAnglesF = numpy.zeros(1000)
 
-amplitudeB = numpy.pi/10
-frequencyB = 30
-phaseOffsetB = -numpy.pi/6
 motorVectorB = numpy.linspace(0,2*numpy.pi,1000)
 targetAnglesB = numpy.zeros(1000)
 
 for i in range(1000):
-    targetAnglesF[i] = amplitudeF * numpy.sin(frequencyF * motorVectorF[i] + phaseOffsetF)
-    targetAnglesB[i] = amplitudeB * numpy.sin(frequencyB * motorVectorB[i] + phaseOffsetB)
+    targetAnglesF[i] = c.amplitudeF * numpy.sin(c.frequencyF * motorVectorF[i] + c.phaseOffsetF)
+    targetAnglesB[i] = c.amplitudeB * numpy.sin(c.frequencyB * motorVectorB[i] + c.phaseOffsetB)
 
 numpy.save("back",targetAnglesB)
 numpy.save("front",targetAnglesF)
@@ -50,3 +46,4 @@ p.disconnect()
 
 numpy.save("data/backLegSensorValues", backLegSensorValues, allow_pickle=True, fix_imports=True)
 numpy.save("data/frontLegSensorValues", frontLegSensorValues, allow_pickle=True, fix_imports=True)
+"""
