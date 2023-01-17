a11d1b8f6a3d3cb5ba2a6d668594c5010d187621
Julia Gangi <juliagangi01@gmail.com>
2023-01-17 13:46:12 -0600

    randomsearch

--- generate.py
+++ generate.py
@@ -1,4 +1,7 @@
 import pyrosim.pyrosim as pyrosim
+from pyrosim.neuralNetwork import NEURAL_NETWORK
+import random
+
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -21,10 +24,15 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
     pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
-    pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 3, weight = 1.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = 1.0)
+    #pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 3, weight = 1.0)
+    #pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = 1.0)
     #pyrosim.Send_Synapse(sourceNeuronName = 0, targetNeuronName = 4, weight = -1.0)
     #pyrosim.Send_Synapse(sourceNeuronName = 2, targetNeuronName = 4, weight = -1.0)
+    sensor_neurons = [0,1,2]
+    motor_neurons = [3,4]
+    for s_neuron in sensor_neurons:
+        for m_neuron in motor_neurons:
+            pyrosim.Send_Synapse(sourceNeuronName = s_neuron, targetNeuronName = m_neuron, weight = random.random()*2-1)
     pyrosim.End()
 
 #Create_World()
