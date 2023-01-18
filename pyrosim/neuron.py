49a13e92aea40c30a5da53b427922a3471664c92
juliagangi <97065262+juliagangi@users.noreply.github.com>
2023-01-18 21:20:42 +0100

    Update neuron.py

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -72,20 +72,13 @@ class NEURON:
         
     def Update_Hidden_Or_Motor_Neuron(self,neurons,synapses):
         
-        self.Set_Value(0)
-        print("name in neuron.py")
-        print(self.name)
-        print(self.value)
         for my_synapse in synapses:
             
             if my_synapse[1] == self.Get_Name():
 
                 self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[my_synapse].Get_Weight(),neurons[my_synapse[0]].Get_Value())
         
-        self.Threshold()
-        print("angle in neuron.py")
-        print(self.value)
-        
+        self.Threshold()        
         
     def Allow_Presynaptic_Neuron_To_Influence_Me(self,weight,value):
         
