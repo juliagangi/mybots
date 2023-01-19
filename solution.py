86ebd015f64e82add96e1c0591c87116839233dc
Julia Gangi <juliagangi01@gmail.com>
2023-01-18 21:21:02 -0600

    hillclimber

---
+++ solution.py
@@ -0,0 +1,43 @@
+import numpy
+import pyrosim.pyrosim as pyrosim
+from pyrosim.neuralNetwork import NEURAL_NETWORK
+import os
+
+
+class SOLUTION:
+    def __init__(self):
+        self.weights = 2*numpy.random.rand(3,2) - 1
+
+    def Evaluate(self):
+        self.Create_World()
+        self.Create_Body()
+        self.Create_Brain()
+        os.system("python3 simulate.py")
+
+    def Create_World(self):
+        pyrosim.Start_SDF("world.sdf")
+        pyrosim.Send_Cube(name="Box", pos=[-3,3,0.5] , size=[1,1,1])
+        pyrosim.End()
+
+    def Create_Body(self):
+        pyrosim.Start_URDF("body.urdf")
+        pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[1,1,1])
+        pyrosim.Send_Joint(name = "Torso_BackLeg", parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
+        pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[1,1,1])
+        pyrosim.Send_Joint(name = "Torso_FrontLeg", parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
+        pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[1,1,1])
+        pyrosim.End()
+
+    def Create_Brain(self):
+        pyrosim.Start_NeuralNetwork("brain.nndf")
+        pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+        pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+        pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
+        pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+        pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
+        sensor_neurons = [0,1,2]
+        motor_neurons = [0,1]
+        for currentRow in sensor_neurons:
+            for currentColumn in motor_neurons:
+                pyrosim.Send_Synapse(sourceNeuronName = currentRow, targetNeuronName = currentColumn+3, weight = self.weights[currentRow][currentColumn])
+        pyrosim.End()
