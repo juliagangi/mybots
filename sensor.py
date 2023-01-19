d3e910cb1cfc24b7da812fdee71cd81baf054701
Julia Gangi <juliagangi01@gmail.com>
2023-01-18 20:31:49 -0600

    synapses

--- sensor.py
+++ sensor.py
@@ -4,7 +4,7 @@ import pyrosim.pyrosim as pyrosim
 class SENSOR:
     def __init__(self,linkName):
         self.linkName = linkName
-        self.values = numpy.zeros(100)
+        self.values = numpy.zeros(1000)
 
     def Get_Value(self, t):
         self.values[t] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
