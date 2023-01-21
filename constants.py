8b759116250081b98629b7cb261f099d410b3474
Julia Gangi <juliagangi01@gmail.com>
2023-01-20 18:56:42 -0600

    hillclimber

--- constants.py
+++ constants.py
@@ -3,3 +3,5 @@ import numpy
 amplitude = numpy.pi/8
 frequency = 10
 offset = 0
+numberOfGenerations = 20
+numTimeSteps = 1000
--- generate.py
+++ generate.py
@@ -2,7 +2,6 @@ import pyrosim.pyrosim as pyrosim
 from pyrosim.neuralNetwork import NEURAL_NETWORK
 import random
 
-
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(name="Box", pos=[-3,3,0.5] , size=[1,1,1])
