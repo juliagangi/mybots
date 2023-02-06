2c465bf57e6b85aeaf595cbddc89fe898790bfd2
juliagangi <97065262+juliagangi@users.noreply.github.com>
2023-02-06 22:14:48 +0100

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-from simulation import SIMULATION
-import solution
-
-if sys.argv[1] == 'random':
-    solution.SOLUTION(0).Start_Simulation('GUI')
-elif sys.argv[1] == 'evolved':
-    os.system("python3 search.py")
