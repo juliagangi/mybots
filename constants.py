c3bbc3838d509433ba8258224531b3f2e4a48261
juliagangi <97065262+juliagangi@users.noreply.github.com>
2023-02-11 02:50:29 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,6 @@
 import numpy
+import random
+
 
 amplitude = numpy.pi/8
 frequency = 10
@@ -6,6 +8,15 @@ offset = 0
 numTimeSteps = 1000
 numberOfGenerations = 10
 populationSize = 10
-numSensorNeurons = 9
-numMotorNeurons = 8
+height = numpy.random.randint(2,4)
+length = numpy.random.randint(height,8)
+numMotorNeurons = height-1 + 4*length
+sensorNeuronsArray = []
+numSensorNeurons = 0
+for i in range(numMotorNeurons+1):
+    if random.randint(0,1):
+        sensorNeuronsArray = sensorNeuronsArray + [1]
+        numSensorNeurons = numSensorNeurons + 1
+    else:
+        sensorNeuronsArray = sensorNeuronsArray + [0]
 motorJointRange = .2
