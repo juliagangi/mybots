3581926b56c0d8df7aee902d2c20b3c7c02cfdce
juliagangi <97065262+juliagangi@users.noreply.github.com>
2023-02-10 18:46:05 -0600

    3d

---
+++ Downloads/mybots-finalProject/constants.py
@@ -0,0 +1,11 @@
+import numpy
+
+amplitude = numpy.pi/8
+frequency = 10
+offset = 0
+numTimeSteps = 1000
+numberOfGenerations = 10
+populationSize = 10
+numSensorNeurons = 9
+numMotorNeurons = 8
+motorJointRange = .2
