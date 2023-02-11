053e2e5f4b19cd6316fa583d5df13bcf9e844b2d
juliagangi <97065262+juliagangi@users.noreply.github.com>
2023-02-10 19:46:36 -0600

    commit

---
+++ Downloads/currentbots/parallelHillClimber.py
@@ -0,0 +1,70 @@
+from solution import SOLUTION
+import constants as c
+import copy
+import os
+
+class PARALLEL_HILL_CLIMBER:
+    def __init__(self):
+        os.system("rm brain*.nndf")
+        os.system("rm fitness*.txt")
+        self.parents = {}
+        self.nextAvailableID = 0
+        for i in range(c.populationSize):
+            self.parents[i] = SOLUTION(self.nextAvailableID)
+            self.nextAvailableID = self.nextAvailableID + 1
+
+    def Evolve(self):
+        self.Evaluate(self.parents)
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.Evaluate(self.children)
+        self.Print()
+        self.Select()        
+
+    def Spawn(self):
+        self.children = {}
+        for parent in self.parents:
+            newChild = copy.deepcopy(self.parents[parent])
+            newChild.Set_ID(self.nextAvailableID)
+            self.children[parent] = newChild
+            self.nextAvailableID = self.nextAvailableID + 1
+
+    def Mutate(self):
+        for child in self.children:
+            self.children[child].Mutate()
+
+    def Evaluate(self,solutions):
+        for parent in solutions:
+            solutions[parent].Start_Simulation("DIRECT")
+        for parent in solutions:
+            solutions[parent].Wait_For_Simulation_To_End("DIRECT")
+
+    def Select(self):
+        for parent in self.parents:
+            if self.children[parent].fitness > self.parents[parent].fitness:
+                self.parents[parent] = self.children[parent]
+
+    def Show_Best(self):
+        bestFitness = -1000
+        bestParent = None
+        for parent in self.parents:
+            currParent = self.parents[parent]
+            currFitness = currParent.fitness
+            if currFitness > bestFitness:
+                bestFitness = currFitness
+                bestParent = currParent
+        bestParent.Start_Simulation("GUI")
+
+    def Print(self):
+        print("\n")
+        for parent in self.parents:
+            print("\n", self.parents[parent].fitness, " | ", self.children[parent].fitness)
+        print("\n")
+
+
+
+
---
+++ Downloads/currentbots/robot.py
@@ -0,0 +1,59 @@
+import pybullet as p
+import pyrosim.pyrosim as pyrosim
+from sensor import SENSOR
+from motor import MOTOR
+from pyrosim.neuralNetwork import NEURAL_NETWORK
+import os
+import constants as c
+
+class ROBOT:
+    def __init__(self,solutionID):
+        self.myID = solutionID
+        self.motors = {}
+        self.robot = p.loadURDF("body.urdf")
+        pyrosim.Prepare_To_Simulate(self.robot)
+        self.Prepare_To_Sense()
+        self.Prepare_To_Act()
+        self.nn = NEURAL_NETWORK("brain" + str(self.myID) + ".nndf")
+        os.system("rm brain" + str(self.myID) + ".nndf")
+
+    def Prepare_To_Sense(self):
+        self.sensors = {}
+        i = 0
+        print(c.numMotorNeurons+1)
+        print(pyrosim.linkNamesToIndices)
+        for linkName in pyrosim.linkNamesToIndices:
+            if c.sensorNeuronsArray[i]:
+                self.sensors[linkName] = SENSOR(linkName)
+            i = i + 1
+
+    def Sense(self,t):
+        for sensor in self.sensors:
+            self.sensors[sensor].Get_Value(t)
+
+    def Prepare_To_Act(self):
+        self.motors = {}
+        for jointName in pyrosim.jointNamesToIndices:
+            self.motors[jointName] = MOTOR(jointName)
+
+    def ACT(self):
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName)*c.motorJointRange
+                self.motors[jointName].Set_Value(self.robot,desiredAngle)
+                
+    def Get_Fitness(self):
+        basePositionAndOrientation = p.getBasePositionAndOrientation(self.robot)
+        basePosition = basePositionAndOrientation[0]
+        xPosition = basePosition[0]
+        fitnessFile = open("tmp" + str(self.myID) + ".txt", "w")
+        os.system("mv tmp" + str(self.myID) + ".txt" " fitness" + str(self.myID) + ".txt")
+        fitnessFile.write(str(xPosition))
+        fitnessFile.close()
+
+    def Think(self):
+        self.nn.Update()
+
+
+
