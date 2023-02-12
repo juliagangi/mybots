2cf993857227c64a6d380014fd8d30054c8c68e9
juliagangi <97065262+juliagangi@users.noreply.github.com>
2023-02-12 02:44:33 +0100

    Delete Downloads directory

--- Downloads/new3d/simulate.py
+++
@@ -1,8 +0,0 @@
-import random
-from solution import SOLUTION
-
-numSims = random.randint(1,9)
-print(numSims)
-for i in range(numSims):
-        solution = SOLUTION(i)
-        solution.Start_Simulation()
