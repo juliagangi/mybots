65e1b821d5989048abe5a188a34891940b039f5b
juliagangi <97065262+juliagangi@users.noreply.github.com>
2023-02-13 20:16:08 -0600

    commit

--- simulation.py
+++ simulation.py
@@ -9,6 +9,7 @@ import time
 class SIMULATION:
     def __init__(self,solutionID):
         physicsClient = p.connect(p.GUI)
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0,0,-9.8)
         self.world = WORLD()
